cc39f8a349b7ab080079c8ca0262b7d4f9b25d9d
illuminatedspace <anfractua@gmail.com>
2016-07-14 21:17:55 -0400

    Changes Jump Animation and player character script

--- Looper Project/Assets/Scripts/PlayerSc.cs
+++ Looper Project/Assets/Scripts/PlayerSc.cs
@@ -7,12 +7,14 @@ public class PlayerSc : MonoBehaviour {
     public float jumpVelocity = 50f; // Variable determining the sudden vertical force applied to the rigidbody.
     public LayerMask groundLayerTest;
 
+    private float moveVertical = 0f;
     private Rigidbody2D rb; // Declares a variable of type RigidBody2D game component.
     private bool facingRight = false;
 	private Animator MyAnimator;
 
 
-	void Start() // This function Attaches this script to the rigidbody that is also a  part of this scripts object.
+
+    void Start() // This function Attaches this script to the rigidbody that is also a  part of this scripts object.
 	{
 		rb = GetComponent<Rigidbody2D>(); // Gets the Rigidbody2D component that is attached to the same asset as this script.
 		MyAnimator = GetComponent<Animator>();
@@ -27,22 +29,22 @@ public class PlayerSc : MonoBehaviour {
 
 
 		float moveHorizontal = Input.GetAxis("Horizontal") * speed;
-		var moveVertical = 0f;
+        moveVertical = 0f;
+        bool isJumping = MyAnimator.GetBool("Jump");
 
 
 		if (Input.GetButtonDown ("Jump") && rb.IsTouchingLayers (groundLayerTest)) {
-			moveVertical = jumpVelocity;
+            Jump();
 		}
 
-		if (!rb.IsTouchingLayers (groundLayerTest)) {
-		MyAnimator.SetBool ("Jump", true);
-            Debug.Log("Jummpin up");
-		} 
-
-		else {
-		MyAnimator.SetBool ("Jump", false);
-            Debug.Log("Player Landed");
-		}
+        //Sets the landing animator bool to true if the sprite is not touching the ground
+        //AND is moving negative vertically
+        if (!rb.IsTouchingLayers (groundLayerTest) && !isJumping)
+        {
+            MyAnimator.SetBool ("Falling", true);
+            Debug.Log("Player Falling");
+        }
+        else { MyAnimator.SetBool("Falling", false); }
 
         if (moveHorizontal != 0)
         {
@@ -75,5 +77,22 @@ public class PlayerSc : MonoBehaviour {
         Vector3 theScale = transform.localScale;
         theScale.x *= -1;
         transform.localScale = theScale;
+    }
+
+    void Jump()
+    {
+        moveVertical = jumpVelocity;
+
+        if (!rb.IsTouchingLayers(groundLayerTest))
+        {
+            MyAnimator.SetBool("Jump", true);
+            Debug.Log("Jummpin up");
+        }
+
+        //else
+        //{
+        //    MyAnimator.SetBool("Jump", false);
+        //    Debug.Log("Player Landed");
+        //}
     }
 }
